dbdf55f06748127dfa6ad9cb166007423cd989a4
Austin <rockmanvnx6@gmail.com>
2019-04-18 15:42:57 +1000

    set up server environtment

--- Canvas/src/js/main.js
+++ Canvas/src/js/main.js
@@ -1,3 +1,8 @@
+import io from 'socket.io-client';
+const socket = io('http://localhost:8080');
 import GameEngine from './GameEngine';
 let main = new GameEngine(document.querySelector("canvas"), 450, 450);
 main.render();
+socket.emit("msg", {
+    msg: "hello mom"
+})
